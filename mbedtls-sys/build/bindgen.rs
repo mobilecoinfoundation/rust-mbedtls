72a0b892524315c2c0cf2aa4c18511d51d534e2e
drakeley <sara.drakeley@gmail.com>
2019-12-13 11:23:30 -0800

    Changes to match mobilecoin at revision 81e636d7c302c1180f874b08f88cdba563eced5d"

--- mbedtls-sys/build/bindgen.rs
+++ mbedtls-sys/build/bindgen.rs
@@ -21,25 +21,28 @@ struct ParseCallbacks {}
 
 impl bindgen::callbacks::ParseCallbacks for ParseCallbacks {
     fn int_macro(&self, name: &str, _value: i64) -> Option<IntKind> {
-        if name.starts_with("MBEDTLS_") {
+        if name.starts_with("MBEDTLS_SSL_IS_") ||
+            name.starts_with("MBEDTLS_SSL_PRESET_") ||
+            name.starts_with("MBEDTLS_SSL_TRANSPORT_") ||
+            name.starts_with("MBEDTLS_SSL_VERIFY_") ||
+            name.starts_with("MBEDTLS_TLS_RSA_WITH_") ||
+            name.starts_with("MBEDTLS_TLS_RSA_PSK_WITH_") ||
+            name.starts_with("MBEDTLS_TLS_ECJPAKE_WITH_") ||
+            name.starts_with("MBEDTLS_TLS_DHE_RSA_WITH_") ||
+            name.starts_with("MBEDTLS_TLS_ECDH_ECDSA_WITH_") ||
+            name.starts_with("MBEDTLS_TLS_ECDHE_ECDSA_WITH_") ||
+            name.starts_with("MBEDTLS_TLS_ECDH_RSA_WITH_") ||
+            name.starts_with("MBEDTLS_TLS_ECDHE_RSA_WITH_") ||
+            name.starts_with("MBEDTLS_TLS_ECDHE_PSK_WITH_") ||
+            name.starts_with("MBEDTLS_TLS_PSK_WITH_") ||
+            name.starts_with("MBEDTLS_TLS_DHE_PSK_WITH_") ||
+            name.starts_with("MBEDTLS_SSL_SESSION_TICKETS_") ||
+            name.starts_with("MBEDTLS_CTR_DRBG_PR_") ||
+            name.starts_with("MBEDTLS_ENTROPY_SOURCE_") ||
+            name.starts_with("MBEDTLS_HMAC_DRBG_PR_") ||
+            name.starts_with("MBEDTLS_RSA_PKCS_")
+             {
             Some(IntKind::Int)
-        } else {
-            None
-        }
-    }
-
-    fn enum_variant_name(
-        &self,
-        _enum_name: Option<&str>,
-        original_variant_name: &str,
-        _variant_value: bindgen::callbacks::EnumVariantValue,
-    ) -> Option<String> {
-        if original_variant_name.starts_with("MBEDTLS_") {
-            Some(
-                original_variant_name
-                    .trim_start_matches("MBEDTLS_")
-                    .to_string(),
-            )
         } else {
             None
         }
@@ -50,18 +53,23 @@ impl bindgen::callbacks::ParseCallbacks for ParseCallbacks {
             if original_item_name == "mbedtls_time_t" {
                 None
             } else {
-                Some(
-                    original_item_name
-                        .trim_start_matches("mbedtls_")
-                        .to_string(),
-                )
+                Some(original_item_name.trim_start_matches("mbedtls_").to_string())
             }
         } else if original_item_name.starts_with("MBEDTLS_") {
-            Some(
-                original_item_name
-                    .trim_start_matches("MBEDTLS_")
-                    .to_string(),
-            )
+            Some(original_item_name.trim_start_matches("MBEDTLS_").to_string())
+        } else {
+            None
+        }
+    }
+
+    fn enum_variant_name(
+        &self,
+        _enum_name: Option<&str>,
+        original_variant_name: &str,
+        _variant_value: bindgen::callbacks::EnumVariantValue
+    ) -> Option<String> {
+        if original_variant_name.starts_with("MBEDTLS_") {
+            Some(original_variant_name.trim_start_matches("MBEDTLS_").to_string())
         } else {
             None
         }
@@ -76,8 +84,7 @@ impl super::BuildConfig {
                 Ok(for h in headers::enabled_ordered() {
                     writeln!(f, "#include <mbedtls/{}>", h)?;
                 })
-            })
-            .expect("bindgen-input.h I/O error");
+            }).expect("bindgen-input.h I/O error");
 
         let include = self.mbedtls_src.join("include");
         let target = env::var("TARGET").expect("TARGET environment variable not set");
@@ -85,92 +92,59 @@ impl super::BuildConfig {
         let bindings = (match target.as_str() {
             /* iOS */
             "armv7-apple-ios" | "armv7s-apple-ios" | "aarch64-apple-ios" => {
-                let ios_sdk_path_output = Command::new("xcrun")
-                    .args(&["--sdk", "iphoneos", "--show-sdk-path"])
-                    .output()
-                    .expect("Failed to get xcode iphoneos sdk path");
+                let ios_sdk_path_output = Command::new("xcrun").args(&["--sdk", "iphoneos", "--show-sdk-path"])
+                    .output().expect("Failed to get xcode iphoneos sdk path");
                 let ios_sdk_path = String::from_utf8(ios_sdk_path_output.stdout)
                     .expect("Command output from xcrun not UTF-8")
-                    .trim()
-                    .to_string();
+                    .trim().to_string();
 
                 (match target.as_str() {
-                    "armv7-apple-ios" | "armv7s-apple-ios" => {
-                        bindgen::builder().clang_args(&["-target", &target])
-                    }
-                    "aarch64-apple-ios" => {
-                        bindgen::builder().clang_args(&["-target", "arm64-apple-ios"])
-                    }
-                    _ => bindgen::builder(),
-                })
-                .clang_args(&["-isysroot", ios_sdk_path.as_str()])
-            }
+                    "armv7-apple-ios" | "armv7s-apple-ios" => bindgen::builder()
+                        .clang_args(&["-target", &target]),
+                    "aarch64-apple-ios" => bindgen::builder()
+                        .clang_args(&["-target", "arm64-apple-ios"]),
+                    _ => bindgen::builder()
+                }).clang_args(&["-isysroot", ios_sdk_path.as_str()])
+            },
             /* Android*/
-            "i686-linux-andoid" | "armv7-linux-androideabi" | "aarch64-linux-android" => {
-                bindgen::builder()
-                    .clang_args(&[
-                        "-isysroot",
-                        &env::var("ISYSROOT")
-                            .expect("Need ISYSROOT env var for Android compilation"),
-                    ])
-                    .clang_args(&[
-                        "-isystem",
-                        &env::var("ISYSTEM").expect("Need ISYSTEM env var for Android compilation"),
-                    ])
-            }
+            "i686-linux-andoid" | "armv7-linux-androideabi" | "aarch64-linux-android" => bindgen::builder()
+                .clang_args(&["-isysroot", &env::var("ISYSROOT").expect("Need ISYSROOT env var for Android compilation")])
+                .clang_args(&["-isystem", &env::var("ISYSTEM").expect("Need ISYSTEM env var for Android compilation")]),
             /* Everything else */
-            _ => bindgen::builder(),
+            _ => bindgen::builder()
         })
-        .clang_arg("-Dmbedtls_t_udbl=mbedtls_t_udbl;") // bindgen can't handle unused uint128
-        .clang_arg(format!(
-            "-DMBEDTLS_CONFIG_FILE=\"{}\"",
-            self.config_h.to_str().expect("config.h UTF-8 error")
-        ))
-        .clang_arg(format!(
-            "-I{}",
-            include.to_str().expect("include/ UTF-8 error")
-        ))
-        .header(
-            header
-                .to_str()
-                .expect("failed to convert header path to string"),
-        )
-        .use_core()
-        .derive_debug(false) // buggy :(
-        .parse_callbacks(Box::new(ParseCallbacks {}))
-        .ctypes_prefix("crate::types::raw_types")
-        .blacklist_function("strtold")
-        .blacklist_function("qecvt_r")
-        .blacklist_function("qecvt")
-        .blacklist_function("qfcvt_r")
-        .blacklist_function("qgcvt")
-        .blacklist_function("qfcvt")
-        .opaque_type("std::*")
-        .opaque_type("time_t")
-        .generate_comments(false)
-        .prepend_enum_name(false)
-        .generate()
-        .expect("bindgen error");
+            .clang_arg("-Dmbedtls_t_udbl=mbedtls_t_udbl;") // bindgen can't handle unused uint128
+            .clang_arg(format!(
+                "-DMBEDTLS_CONFIG_FILE=\"{}\"",
+                self.config_h.to_str().expect("config.h UTF-8 error")
+            )).clang_arg(format!(
+                "-I{}",
+                include.to_str().expect("include/ UTF-8 error")
+            )).header(
+                header
+                    .to_str()
+                    .expect("failed to convert header path to string"),
+            ).use_core()
+            .derive_debug(false) // buggy :(
+            .disable_name_namespacing()
+            .prepend_enum_name(false)
+            .ctypes_prefix("raw_types")
+            .parse_callbacks(Box::new(ParseCallbacks{}))
+            // max_align_t is causing bindgen generated tests to fail an alignment check and
+            // is not needed by the bindings.
+            .blacklist_type("max_align_t")
+            // Including the comments breaks the generated code because it contains formatting
+            // that is interpreted as escaped characters.
+            .generate_comments(false)
+            .generate()
+            .expect("bindgen error");
 
         let bindings_rs = self.out_dir.join("bindings.rs");
         File::create(&bindings_rs)
             .and_then(|mut f| {
                 bindings.write(Box::new(&mut f))?;
-                f.write_all(
-                    br#"
-                #[allow(nonstandard_style)]
-                #[allow(unused_imports)]
-                "#,
-                )?;
-
-                f.write_all(
-                    br#"
-                // for FILE, time_t, etc
-                use crate::types::*;
-                "#,
-                )
-            })
-            .expect("bindings.rs I/O error");
+                f.write_all(b"use crate::types::*;\n") // for FILE, time_t, etc.
+            }).expect("bindings.rs I/O error");
 
         let mod_bindings = self.out_dir.join("mod-bindings.rs");
         File::create(&mod_bindings)
--- mbedtls/tests/client_server.rs
+++ mbedtls/tests/client_server.rs
@@ -10,7 +10,7 @@
 
 extern crate mbedtls;
 
-use genio::{Read, Write};
+use std::io::{Read, Write};
 use std::net::TcpStream;
 
 use mbedtls::pk::Pk;
@@ -29,8 +29,7 @@ fn client(
     mut conn: TcpStream,
     min_version: Version,
     max_version: Version,
-    exp_version: Option<Version>,
-) -> TlsResult<()> {
+    exp_version: Option<Version>) -> TlsResult<()> {
     let mut entropy = entropy_new();
     let mut rng = CtrDrbg::new(&mut entropy, None)?;
     let mut cacert = Certificate::from_pem(keys::ROOT_CA_CERT)?;
@@ -43,7 +42,7 @@ fn client(
             &mut |crt: &mut LinkedCertificate, depth, verify_flags: &mut VerifyError| {
                 verify_args = Some((crt.subject().unwrap(), depth, *verify_flags));
                 verify_flags.remove(VerifyError::CERT_EXPIRED); //we check the flags at the end,
-                                                                //so removing this flag here prevents the connections from failing with VerifyError
+                //so removing this flag here prevents the connections from failing with VerifyError
                 Ok(())
             };
         let mut config = Config::new(Endpoint::Client, Transport::Stream, Preset::Default);
@@ -63,8 +62,8 @@ fn client(
             }
             Err(e) => {
                 match e {
-                    Error::SslBadHsProtocolVersion => assert!(exp_version.is_none()),
-                    Error::SslFatalAlertMessage => {}
+                    Error::SslBadHsProtocolVersion => {assert!(exp_version.is_none())},
+                    Error::SslFatalAlertMessage => {},
                     e => panic!("Unexpected error {}", e),
                 };
                 return Ok(());
@@ -79,10 +78,7 @@ fn client(
         session.read_exact(&mut buf).unwrap();
         assert_eq!(&buf, format!("Server2Client {:4x}", ciphersuite).as_bytes());
     } // drop verify_callback, releasing borrow of verify_args
-    assert_eq!(
-        verify_args,
-        Some((keys::EXPIRED_CERT_SUBJECT.to_owned(), 0, expected_flags))
-    );
+    assert_eq!(verify_args, Some((keys::EXPIRED_CERT_SUBJECT.to_owned(), 0, expected_flags)));
     Ok(())
 }
 
@@ -112,8 +108,8 @@ fn server(
         Err(e) => {
             match e {
                 // client just closes connection instead of sending alert
-                Error::NetSendFailed => assert!(exp_version.is_none()),
-                Error::SslBadHsProtocolVersion => {}
+                Error::NetSendFailed => {assert!(exp_version.is_none())},
+                Error::SslBadHsProtocolVersion => {},
                 e => panic!("Unexpected error {}", e),
             };
             return Ok(());
@@ -139,7 +135,7 @@ mod test {
     fn client_server_test() {
         use mbedtls::ssl::Version;
 
-        #[derive(Copy, Clone)]
+        #[derive(Copy,Clone)]
         struct TestConfig {
             min_c: Version,
             max_c: Version,
@@ -149,80 +145,20 @@ mod test {
         }
 
         impl TestConfig {
-            pub fn new(
-                min_c: Version,
-                max_c: Version,
-                min_s: Version,
-                max_s: Version,
-                exp_ver: Option<Version>,
-            ) -> Self {
-                TestConfig {
-                    min_c,
-                    max_c,
-                    min_s,
-                    max_s,
-                    exp_ver,
-                }
+            pub fn new(min_c: Version, max_c: Version, min_s: Version, max_s: Version, exp_ver: Option<Version>) -> Self {
+                TestConfig { min_c, max_c, min_s, max_s, exp_ver }
             }
         }
 
         let test_configs = [
-            TestConfig::new(
-                Version::Ssl3,
-                Version::Ssl3,
-                Version::Ssl3,
-                Version::Ssl3,
-                Some(Version::Ssl3),
-            ),
-            TestConfig::new(
-                Version::Ssl3,
-                Version::Tls1_2,
-                Version::Ssl3,
-                Version::Ssl3,
-                Some(Version::Ssl3),
-            ),
-            TestConfig::new(
-                Version::Tls1_0,
-                Version::Tls1_0,
-                Version::Tls1_0,
-                Version::Tls1_0,
-                Some(Version::Tls1_0),
-            ),
-            TestConfig::new(
-                Version::Tls1_1,
-                Version::Tls1_1,
-                Version::Tls1_1,
-                Version::Tls1_1,
-                Some(Version::Tls1_1),
-            ),
-            TestConfig::new(
-                Version::Tls1_2,
-                Version::Tls1_2,
-                Version::Tls1_2,
-                Version::Tls1_2,
-                Some(Version::Tls1_2),
-            ),
-            TestConfig::new(
-                Version::Tls1_0,
-                Version::Tls1_2,
-                Version::Tls1_0,
-                Version::Tls1_2,
-                Some(Version::Tls1_2),
-            ),
-            TestConfig::new(
-                Version::Tls1_2,
-                Version::Tls1_2,
-                Version::Tls1_0,
-                Version::Tls1_2,
-                Some(Version::Tls1_2),
-            ),
-            TestConfig::new(
-                Version::Tls1_0,
-                Version::Tls1_1,
-                Version::Tls1_2,
-                Version::Tls1_2,
-                None,
-            ),
+            TestConfig::new(Version::Ssl3, Version::Ssl3, Version::Ssl3, Version::Ssl3, Some(Version::Ssl3)),
+            TestConfig::new(Version::Ssl3, Version::Tls1_2, Version::Ssl3, Version::Ssl3, Some(Version::Ssl3)),
+            TestConfig::new(Version::Tls1_0, Version::Tls1_0, Version::Tls1_0, Version::Tls1_0, Some(Version::Tls1_0)),
+            TestConfig::new(Version::Tls1_1, Version::Tls1_1, Version::Tls1_1, Version::Tls1_1, Some(Version::Tls1_1)),
+            TestConfig::new(Version::Tls1_2, Version::Tls1_2, Version::Tls1_2, Version::Tls1_2, Some(Version::Tls1_2)),
+            TestConfig::new(Version::Tls1_0, Version::Tls1_2, Version::Tls1_0, Version::Tls1_2, Some(Version::Tls1_2)),
+            TestConfig::new(Version::Tls1_2, Version::Tls1_2, Version::Tls1_0, Version::Tls1_2, Some(Version::Tls1_2)),
+            TestConfig::new(Version::Tls1_0, Version::Tls1_1, Version::Tls1_2, Version::Tls1_2, None)
         ];
 
         for config in &test_configs {
@@ -232,9 +168,7 @@ mod test {
             let max_s = config.max_s;
             let exp_ver = config.exp_ver;
 
-            if (max_c < Version::Tls1_2 || max_s < Version::Tls1_2)
-                && !cfg!(feature = "legacy_protocols")
-            {
+            if (max_c < Version::Tls1_2 || max_s < Version::Tls1_2) && !cfg!(feature = "legacy_protocols") {
                 continue;
             }
 
